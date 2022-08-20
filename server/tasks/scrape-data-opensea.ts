8fdf913a270c9490b9863e78c227b4d1bb967af4
Admazzola <admazzola@gmail.com>
2022-08-19 23:29:45 -0400

    can scrape opensea

--- server/tasks/scrape-data-opensea.ts
+++ server/tasks/scrape-data-opensea.ts
@@ -12,44 +12,68 @@ export async function scrapeDataOpensea(query, mongoDB:ExtensibleMongoDB){
         headers:{
             "X-API-KEY": process.env.OPENSEA_API_KEY
         }
+    }  
+
+    let active = true 
+
+    let nextCursor = undefined 
+    
+    while(active){
+
+        let uri = "https://api.opensea.io/api/v1/assets?"
+        uri = uri.concat(`collection=${query.collectionName}`)
+        uri = uri.concat("&limit=50")
+        
+        if(nextCursor){
+            uri = uri.concat(`&cursor=${nextCursor}`)
+        }
+
+        let response = await resolveGetQueryAsserted(uri,options)
+
+        console.log({response})
+    
+        nextCursor = response.next 
+        
+        let assets = response.assets
+
+        //console.log(JSON.stringify(assets))
+
+        //@ts-ignore
+        let digitalAssetController = new DigitalAssetController(mongoDB, {})
+
+        for(let asset of assets){
+
+            let name = asset.name 
+            let nameSimple = asset.name.split('#')[0].trim()
+
+            let image_url = asset.image_url
+            let image_preview_url = asset.image_preview_url 
+
+            await digitalAssetController.insertNewDigitalAsset({
+                parentUserId: undefined,
+                title: nameSimple,
+                contractAddress: asset.asset_contract.address ,
+                primaryTokenId: asset.token_id,
+                metadataURI: asset.token_metadata
+
+
+
+            })
+        }
+
+
+        await sleep(1000)
+
+
     }
- 
 
-    let uri = "https://api.opensea.io/api/v1/assets?"
-    uri = uri.concat(`collection=${query.collectionName}`)
-    uri = uri.concat("&limit=50")
+   
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-    let response = await resolveGetQueryAsserted(uri,options)
-
-    console.log({response})
- 
-
-    let assets = response.assets
-
-    //console.log(JSON.stringify(assets))
-
-     //@ts-ignore
-    let digitalAssetController = new DigitalAssetController(mongoDB, {})
-
-    for(let asset of assets){
-
-        let name = asset.name 
-        let nameSimple = asset.name.split('#')[0].trim()
-
-        let image_url = asset.image_url
-        let image_preview_url = asset.image_preview_url 
-
-        await digitalAssetController.insertNewDigitalAsset({
-            parentUserId: undefined,
-            title: nameSimple,
-            contractAddress: asset.asset_contract.address ,
-            primaryTokenId: asset.token_id,
-            metadataURI: asset.token_metadata
-
-
-
-        })
-    }
     
 
 }
