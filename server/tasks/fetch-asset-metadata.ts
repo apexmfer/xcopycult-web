24eec0839915e8a4315d351615265ba51e51fc5d
Admazzola <admazzola@gmail.com>
2022-08-23 11:44:58 -0400

    resized

--- server/tasks/fetch-asset-metadata.ts
+++ server/tasks/fetch-asset-metadata.ts
@@ -93,12 +93,17 @@ export async function fetchAssetMetadata( args: string[], mongoDB:ExtensibleMong
         
         let downloadedImageDataBuffer:Buffer = await FileHelper.downloadImageToBinary(  imageURL )
         
-         
-        let resizedBuffer = await gifResize(
-            {  width: 260 }
-            )(downloadedImageDataBuffer) 
-           
+        let resizedImageBuffer = await new Promise( (resolve,reject) => {
 
+             gifResize(
+                {  width: 260 }
+                )(downloadedImageDataBuffer).then(data => {
+                    resolve(data)
+                }  )
+
+
+        })
+         
 
         let newImageRecord = await AttachedImageController.uploadNewImage( downloadedImageDataBuffer, imageTitle, extension, 'primary', mongoDB  )
         let attach = await AttachedImageController.attachImage(newImageRecord.data._id, "digitalasset", nextAsset.data._id , mongoDB)
@@ -107,17 +112,19 @@ export async function fetchAssetMetadata( args: string[], mongoDB:ExtensibleMong
         console.log({newImageRecord})
         console.log({attach})
 
-        let newThumbnailImageRecord = await AttachedImageController.uploadNewImage( resizedBuffer, imageTitle, extension, 'thumbnail', mongoDB  )
+        let newThumbnailImageRecord = await AttachedImageController.uploadNewImage( resizedImageBuffer, imageTitle, extension, 'thumbnail', mongoDB  )
         let attachThumbnail = await AttachedImageController.attachImage(newThumbnailImageRecord.data._id, "digitalasset", nextAsset.data._id , mongoDB)
 
 
+        console.log({newThumbnailImageRecord})
+        console.log({attachThumbnail})
         //link image to this asset 
 
 
     }catch(e){
 
         console.error(e)
-        
+
         await digitalAssetController.updateDigitalAsset({
             assetId: nextAsset.data._id,
             modifyParams: { metadataCached: errorMessage }  
