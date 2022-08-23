f0b251ed5d3b258751aee1afaeed3c30eec2b81b
Admazzola <admazzola@gmail.com>
2022-08-23 13:19:44 -0400

    footer

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="section py-4 text-white h-full bg-neutral-800" style="  min-height: 250px;">
-    <div class=" h-full ">
-      <div class=" flex flex-row h-full">
+    <div class=" h-full flex flex-col" style="min-height: 250px;">
+      <div class=" flex flex-grow flex-row  ">
          
-        <div class="w-1/3 ">
+        <div class="w-1/3 p-2 ">
           <h3 class=" text-md   ">Documentation</h3>
 
         <a href="https://github.com/apexmfer/xcopycult-web" target="_blank"> Source Code </a>
          
            
           </div>
-        <div class="w-1/3 h-full">
+        <div class="w-1/3 h-full p-2">
           <h3 class=" text-md  ">Community &amp; Social</h3>
 
           <a href="https://xcopy.art/" target="_blank"> XCOPY Art </a>
-         
+        
          
          </div>
 
@@ -25,6 +25,13 @@
 
 
       </div>
+       <div class=" flex flex-row w-full" style="height:60px;">
+
+
+        <div class="text-center w-full"> Built by  <a href="https://twitter.com/ethereumdegen"> @EthereumDegen </a></div>
+         
+      </div>
+
     </div>
   </div>
 </template>
