83607bf0652d68e65dadb861d1269311dbdf3d57
Admazzola <admazzola@gmail.com>
2022-08-23 12:40:08 -0400

    footer

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -2,17 +2,16 @@
   <div class="section py-4 text-white h-full bg-neutral-800" style="  min-height: 250px;">
     <div class=" h-full ">
       <div class=" flex flex-row h-full">
-         <div class="w-1/4 ">
-         </div>
-        <div class="w-1/4 ">
-          <h3 class=" text-lg   ">Documentation</h3>
+         
+        <div class="w-1/3 ">
+          <h3 class=" text-md   ">Documentation</h3>
 
         <a href="https://github.com/apexmfer/xcopycult-web" target="_blank"> Source Code </a>
          
            
           </div>
-        <div class="w-1/4 h-full">
-          <h3 class=" text-lg  ">Community &amp; Social</h3>
+        <div class="w-1/3 h-full">
+          <h3 class=" text-md  ">Community &amp; Social</h3>
 
           <a href="https://xcopy.art/" target="_blank"> XCOPY Art </a>
          
@@ -20,7 +19,7 @@
          </div>
 
 
-           <div class="w-1/4 text-gray-500 p-4">
+           <div class="w-1/3 text-gray-500 p-4">
            XCOPYCULT is not officially affiliated with XCOPY
          </div>
 
